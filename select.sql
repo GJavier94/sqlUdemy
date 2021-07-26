4d52bc3bca906cf377aedda652311e953137ccad
Javier Armenta <javarmgar@gmail.com>
2021-07-25 20:54:16 -0500

    basic select sql for customers

---
+++ select.sql
@@ -0,0 +1,18 @@
+use Sales;
+
+select * from Customers;
+select * from Sales.Customers;
+
+select * from Sales;
+select * from Sales.Sales;
+
+/*
+Para eliminar una tabla
+*/
+drop table Sales; 
+
+
+drop table Customers;
+
+
+
